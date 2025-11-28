f444b036b6d923432df1e270fde996d13a5e179a
SithScripter <forwardtoanuj@gmail.com>
2025-11-28 14:56:00 +0530

    Refactor

--- src/test/java/com/demo/flightbooking/tests/api/SampleApiTest.java
+++ src/test/java/com/demo/flightbooking/tests/api/SampleApiTest.java
@@ -1,29 +1,41 @@
 package com.demo.flightbooking.tests.api;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.demo.flightbooking.tests.base.BaseTest;
+import com.demo.flightbooking.utils.ExtentManager;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.Matchers.equalTo;
 
-public class SampleApiTest {
+public class SampleApiTest extends BaseTest {
 
     @Test(description = "Perform a sample API test to demonstrate API testing capability.")
     public void sampleApiTest() {
-        // Set the base URI for the API
+        ExtentTest test = ExtentManager.getTest();
+        if (test != null) {
+            test.log(Status.INFO, "Starting API validation against ReqRes");
+        }
+
         RestAssured.baseURI = "https://reqres.in/api";
 
-        // Perform a GET request to the /users/2 endpoint
         Response response = RestAssured.given()
                 .when()
                 .get("/users/2")
                 .then()
-                .statusCode(200) // Assert that the status code is 200 OK
-                .body("data.id", equalTo(2)) // Assert that the user ID in the response is 2
-                .body("data.email", equalTo("janet.weaver@reqres.in")) // Assert the email
+                .statusCode(200)
+                .body("data.id", equalTo(2))
+                .body("data.email", equalTo("janet.weaver@reqres.in"))
                 .extract().response();
 
-        // Print the response body to the console for verification
-        System.out.println("Response Body: " + response.getBody().asString());
+        String responseBody = response.getBody().asString();
+        System.out.println("Response Body: " + responseBody);
+
+        if (test != null) {
+            test.log(Status.PASS, "Verified ReqRes user API response for user 2");
+            test.log(Status.INFO, "Payload: " + responseBody);
+        }
     }
 }
